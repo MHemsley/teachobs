a1c2df1d229377f75517e4d8409d29499c0de50e
Elias <elias0789@gmail.com>
2016-02-19 14:00:45 +0000

    Get some basic routing and html up

--- controllers/routing.js
+++ controllers/routing.js
@@ -1,5 +1,56 @@
-function router(req, res) {
+var fs = require('fs');
 
+function getRouter(req, res) {
+    if (req.url === '/') {
+        fs.readFile(__dirname + '/../views/index.html', function(error, content) {
+            if (error) {
+                console.log(error);
+                res.writeHead(500, {'Content-Type': 'text/html'});
+                res.end(error.toString('utf8'));
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(content.toString('utf8'));
+            }
+        });
+    } else if (req.url.search('/assessment') === 0) {
+        //
+    } else if (req.url.search('/review') === 0) {
+        //
+    } else if (req.url.indexOf('.') > -1) {
+        var ext = req.url.split('.')[1];
+        fs.readFile(__dirname + '/../public/' + req.url, function(error, content) {
+            if (error) {
+                console.log(error);
+                res.writeHead(500, {'Content-Type': 'text/html'});
+                res.end(error.toString('utf8'));
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/' + ext});
+                res.end(content.toString('utf8'));
+            }
+        });
+
+    } else { // 404
+
+    }
+}
+
+function postRouter(req, res) {
+    if (req.url.search('/submit') === 0) {
+
+    } else { // 404
+
+    }
+}
+
+function router(req, res) {
+    if (req.method === "GET") {
+        getRouter(req, res);
+    } else if (req.method === "POST") {
+        postRouter(req, res);
+    } else {
+        res.writeHead(400, {'Content-Type': 'text/html'});
+        res.end('Unsupported HTTP method');
+    }
 }
 
 module.exports = {
